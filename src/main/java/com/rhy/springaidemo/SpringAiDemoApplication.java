7bde4a6d7f4391e71a2b91b9278ac0c3fae3f21b
rhy <415560124@qq.com>
2025-01-02 22:09:18 +0800

    1.添加PgVector功能

--- src/main/java/com/rhy/springaidemo/SpringAiDemoApplication.java
+++ src/main/java/com/rhy/springaidemo/SpringAiDemoApplication.java
@@ -1,9 +1,12 @@
 package com.rhy.springaidemo;
 
+import com.alibaba.druid.spring.boot3.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {DruidDataSourceAutoConfigure.class}
+)
 public class SpringAiDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/rhy/springaidemo/controller/OllamaController.java
+++ src/main/java/com/rhy/springaidemo/controller/OllamaController.java
@@ -1,18 +1,58 @@
 package com.rhy.springaidemo.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.pgvector.PGvector;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ollama")
 public class OllamaController {
     @Autowired
     private OllamaChatModel ollamaChatModel;
-    @RequestMapping("/test")
+
+    @Autowired
+    private OllamaEmbeddingModel ollamaEmbeddingModel;
+
+    @Autowired
+    private VectorStore vectorStore;
+
+    @GetMapping("/test")
     public String call(@RequestParam("message") String message){
         return ollamaChatModel.call(message);
     }
+
+    @GetMapping("/embedding")
+    public String embedding(@RequestParam("message") String message){
+        float[] embed = ollamaEmbeddingModel.embed(message);
+        return JSONObject.toJSONString(embed);
+    }
+    @PostMapping("/embedding/store")
+    public String embeddingStore(@RequestParam("message") String message){
+        List<Document> documents = List.of(
+                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
+                new Document("The World is Big and Salvation Lurks Around the Corner"),
+                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
+
+        // Add the documents to PGVector
+        vectorStore.add(documents);
+
+        // Retrieve documents similar to a query
+        List<Document> results = this.vectorStore.similaritySearch(SearchRequest.builder().query("Spring").topK(5).build());
+        return JSONObject.toJSONString(results);
+    }
+
+
 }
